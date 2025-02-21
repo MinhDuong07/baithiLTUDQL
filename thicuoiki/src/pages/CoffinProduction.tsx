f03f1c70a701bd3d30fa929b4ad16b95dba3aa47
MinhDuong07 <nguyenminhduong525@gmail.com>
2025-02-21 16:26:43 +0700

    update trang mới

---
+++ thicuoiki/src/pages/CoffinProduction.tsx
@@ -0,0 +1,152 @@
+import { Typography, Timeline } from "antd";
+
+const { Title, Paragraph } = Typography;
+
+function CoffinProduction() {
+    return (
+        <div style={{ padding: "20px", maxWidth: "1300px", margin: "auto" }}>
+            <Paragraph style={{ fontSize: "20px" }}>
+            Với kim chỉ nam “ Tận tâm – Lịch sự - nhã nhặn – thân thiện”, khi đến với cơ sở mai táng Thiện Đức, quý khách hàng thật sự sẽ luôn hài lòng với sự tận tâm của Thiện Đức dành cho quý khách hàng. Bên cạnh với việc cung cấp các gói dịch vụ tang lễ trọn gói khiến rất nhiều gia đình hài lòng. Cơ sơ mai táng Thiện Đức còn là nơi tự sản xuất và cung cấp các sản phẩm quan tài, giúp hỗ trợ cho quý khách hàng với giá phải chăng nhất.
+            Cùng đến với bài viết này của cơ sở mai táng Thiện Đức, để quý khách hàng có thể hiểu hơn về quy trình sản xuất quan tài tại cơ sở mai táng Thiện Đức.
+            Với mỗi sản phẩm quan tài của Thiện Đức đều phải trải qua đầy đủ quy trình sản xuất nghiêm ngặt, kiểm soát chặt chẽ về mặt chất lượng sản phẩm, đảm bảo đem lại sự hài lòng tối đa cho mọi khách hàng.
+            </Paragraph>
+            <Title level={1} style={{ textAlign: "left", fontSize: "30px" }}>Quy trình sản xuất quan tài tại Thiện Đức</Title>
+            <Paragraph style={{ fontSize: "20px" }}>
+            Bước 1: Tư vấn khách hàng – lập lệnh sản xuất
+            </Paragraph>
+            <div
+                style={{
+                display: "flex",
+                justifyContent: "center",
+                marginTop: "20px",
+                }}
+            >
+                <img
+                src="https://traihomthienduc.com/upload/images/quy-trinh-san-xuat-quan-tai-1.png"
+                alt="Poster 1"
+                style={{
+                    width: "100%", // Chiếm toàn bộ chiều rộng container
+                    maxWidth: "800px", // Giới hạn kích thước tối đa
+                    borderRadius: "10px",
+                    boxShadow: "0px 4px 8px rgba(0, 0, 0, 0.2)",
+                }}
+                />
+            </div>
+
+
+            <Paragraph style={{ fontSize: "20px" }}>
+                Căn cứ vào nhu cầu thị hiếu của thị trường mà cơ sở mai táng Thiện Đức sẽ thiết kế và sản xuất các loại quan tài khác nhau với nhiều sự lựa chọn về chất lượng gỗ, thiết kế, giá cả phù hợp với mọi nhu cầu. Hoặc sẽ làm theo yêu cầu trực tiếp từ khách hàng.
+            </Paragraph>
+            <Paragraph style={{ fontSize: "20px" }}>
+                - Kiểm tra chất lượng gỗ
+            </Paragraph>
+            <Paragraph style={{ fontSize: "20px" }}>
+                - Kiểm tra lại thiết kế về các thông tin về chủng loại gỗ, kích thước, màu sắc,...
+            </Paragraph>
+            <Paragraph style={{ fontSize: "20px" }}>
+                - Sau khi kiểm tra, chủ cơ sở mai táng Thiện Đức lập Lệnh sản xuất và yêu cầu các bộ phận bên dưới thực hiện.
+            </Paragraph>
+            <Paragraph style={{ fontSize: "20px" }}>
+                Bước 2: Thực hiện lệnh sản xuất tại cơ sở sản xuất quan tài Thiện Đức
+            </Paragraph>
+            <div
+                style={{
+                display: "flex",
+                justifyContent: "center",
+                marginTop: "20px",
+                }}
+            >
+                <img
+                src="https://traihomthienduc.com/upload/images/quy-trinh-san-xuat-quan-tai-2.png"
+                alt="Poster 2"
+                style={{
+                    width: "100%", // Chiếm toàn bộ chiều rộng container
+                    maxWidth: "800px", // Giới hạn kích thước tối đa
+                    borderRadius: "10px",
+                    boxShadow: "0px 4px 8px rgba(0, 0, 0, 0.2)",
+                }}
+                />
+            </div>
+            <Paragraph style={{ fontSize: "20px" }}>
+                Sau khi nhận được lệnh sản xuất, kỹ thuật/thợ tại cơ sở tiến hành:
+            </Paragraph>
+            <Paragraph style={{ fontSize: "20px" }}>
+                -  Kiểm tra thiết kế ước tính lượng vật tư theo định mức, kiểm tra lượng vật tư.
+            </Paragraph>
+            <Paragraph style={{ fontSize: "20px" }}>
+                -  Tách khối lượng công việc cho từng nhóm theo chức năng của mỗi nhóm sản xuất.
+            </Paragraph>
+            <Paragraph style={{ fontSize: "20px" }}>
+                Bước 3: Kiểm tra chất lượng gia công
+            </Paragraph>
+            <div
+                style={{
+                display: "flex",
+                justifyContent: "center",
+                marginTop: "20px",
+                }}
+            >
+                <img
+                src="https://traihomthienduc.com/upload/images/quy-trinh-san-xuat-quan-tai-3.png"
+                alt="Poster 3"
+                style={{
+                    width: "100%", // Chiếm toàn bộ chiều rộng container
+                    maxWidth: "800px", // Giới hạn kích thước tối đa
+                    borderRadius: "10px",
+                    boxShadow: "0px 4px 8px rgba(0, 0, 0, 0.2)",
+                }}
+                />
+            </div>
+            <Paragraph style={{ fontSize: "20px" }}>
+                Mỗi nhóm sau khi hoàn thành công việc của mình, chủ cơ sở mai táng Thiện Đức trực tiếp kiểm tra Chất lượng gia công sản phẩm:
+            </Paragraph>
+            <Paragraph style={{ fontSize: "20px" }}>
+                -  Kiểm tra chất liệu, chủng loại gỗ, kích thước, kết cấu và chất lượng bề mặt.
+            </Paragraph>
+            <Paragraph style={{ fontSize: "20px" }}>
+                -  Sau khi kiểm tra chất lượng gia công, nếu phần nào chưa đạt yêu cầu sẽ yêu cầu nhóm đó làm lại. Nếu sản phẩm đạt yêu cầu sẽ được duyệt và lưu kho.
+            </Paragraph>
+            <Paragraph style={{ fontSize: "20px" }}>
+                Bước 4: Đóng gói lưu kho
+            </Paragraph>
+            <Paragraph style={{ fontSize: "20px" }}>
+                Quan tài sau khi được kiểm tra chất lượng sẽ được bọc màng xốp, đóng hộp cẩn thận và sẽ được lưu trữ tại kho lưu trữ của cơ sở mai táng Thiện Đức.
+            </Paragraph>
+            <Paragraph style={{ fontSize: "20px" }}>
+                Cơ sở mai táng Thiện Đức – Nơi sản xuất quan tài uy tín được quý khách hàng tin chọn.
+            </Paragraph>
+            <Paragraph style={{ fontSize: "20px" }}>
+                Với các sản phẩm quan tài tại cơ sở mai táng Thiện Đức, quý khách hàng có thể yên tâm bởi các sản phẩm của Thiện Đức luôn trải qua những khâu kiểm tra đầy đủ và bài bản, được kiểm tra chất lượng bởi chủ cơ sở mai táng Thiện Đức là người có nhiều kinh nghiệm trong ngành sản xuất quan tài.
+            </Paragraph>
+            <Paragraph style={{ fontSize: "20px" }}>
+                Bên cạnh đó, với các cơ sở mai táng dịch vụ khác bạn sẽ phải tốn một khoảng tiền không nhỏ bởi dịch vụ cung cấp quan tài sẽ đến từ đơn vị thứ 3. Còn đối với cơ sở mai táng Thiện Đức, bạn có thể lựa chọn các sản phẩm quan tài chính gốc của cơ sở sản xuất, giá thành sẽ bình ổn và phù hợp hơn với giá gốc của sản phẩm. 
+            </Paragraph>
+            <Paragraph style={{ fontSize: "20px" }}>
+                Thiện Đức mong muốn được cùng đồng hành với các gia đình chia sẻ nổi buồn bằng việc tổ chức tang lễ thật chỉnh chu và hoàn thiện để gia đình yên tâm đưa tiễn người thân đã mất về cõi vĩnh hằng.
+            </Paragraph>
+            <Paragraph style={{ fontSize: "20px" }}>
+                Vì sao gia đình bạn nên lựa chọn dịch vụ tại Trại Hòm Thiện Đức?
+            </Paragraph>
+            <Paragraph style={{ fontSize: "20px" }}>
+                Nơi luôn đặt lợi ích của khách hàng lên hàng đầu – Cam kết 100% hài lòng
+            </Paragraph>
+            <Paragraph style={{ fontSize: "20px" }}>
+                Báo gói dịch vụ rõ ràng, minh bạch, bán đúng sản phẩm gia đình chọn
+            </Paragraph>
+            <Paragraph style={{ fontSize: "20px" }}>
+                Với những dịch vụ mai táng chuyên nghiệp, chỉnh chu giúp cho lễ an táng của gia chủ thật sự trang trọng.
+            </Paragraph>
+            <Paragraph style={{ fontSize: "20px" }}>
+                Giá cả phù hợp với những chi phí tại gốc, không qua trung gian, Thiện Đức nhận thanh toán sau tang lễ
+            </Paragraph>
+            <Paragraph style={{ fontSize: "20px" }}>
+                Đúng giờ trong các lễ nghi của lễ an táng.
+            </Paragraph>
+            <Paragraph style={{ fontSize: "20px" }}>
+                Với sứ mệnh: “Tận tâm – Lịch sự - nhã nhặn – thân thiện”. Trại hòm Thiện Đức rất mong được phục vụ, chia sẻ cùng nỗi buồn với gia quyến trong những ngày đau buồn của gia đình. Để được biết thêm thông tin chi tiết về sản phẩm dịch vụ tại trại hòm Thiện Đức quý khách hàng vui lòng liên hệ qua hotline 0902.99.40.99 hoặc fanpage của Trại Hòm Thiện Đức
+            </Paragraph>
+        </div>
+    );
+}
+
+export default CoffinProduction;
